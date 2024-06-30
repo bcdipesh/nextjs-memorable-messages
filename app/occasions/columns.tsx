c2061df2b4a86dd5cf0173efc85b8b19553edafe
Dipesh B C <bcdipeshwork@gmail.com>
2024-06-30 10:59:42 -0400

    Add occasions page

---
+++ app/occasions/columns.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { formatDate } from "@/lib/utils";
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import Link from "next/link";
+
+export type Occasion = {
+  id: string;
+  occasionType: string;
+  message: string;
+  receiverEmail: string;
+  deliveryMethod: string;
+  deliveryDate: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+};
+
+export const columns: ColumnDef<Occasion>[] = [
+  {
+    header: "#",
+    cell: ({ row }) => row.index + 1,
+  },
+  {
+    accessorKey: "occasionType",
+    header: "Occasion Type",
+    cell: ({ row }) => {
+      const { id, occasionType } = row.original;
+
+      return (
+        <Button variant="link" asChild>
+          <Link href={`/occasions/${id}`}>{occasionType}</Link>
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "receiverEmail",
+    header: "Email",
+  },
+  {
+    accessorKey: "deliveryMethod",
+    header: "Delivery Method",
+  },
+  {
+    accessorKey: "deliveryDate",
+    header: "Delivery Date",
+    cell: ({ row }) => {
+      const isoDate = new Date(row.getValue("deliveryDate"));
+      const formattedDate = formatDate(isoDate);
+
+      return <div>{formattedDate}</div>;
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created At",
+    cell: ({ row }) => {
+      const isoDate = new Date(row.getValue("createdAt"));
+      const formattedDate = formatDate(isoDate);
+
+      return <div>{formattedDate}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const occasion = row.original;
+
+      return (
+        <Dialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 2-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => navigator.clipboard.writeText(occasion.id)}
+              >
+                Copy occasion ID
+              </DropdownMenuItem>
+              <DropdownMenuItem>View/Update occasion</DropdownMenuItem>
+              <DialogTrigger asChild>
+                <DropdownMenuItem>Delete occasion</DropdownMenuItem>
+              </DialogTrigger>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. Are you sure you want to
+                permanently delete this occasion?
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <Button type="submit">Confirm</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      );
+    },
+  },
+];
--- app/occasions/page.tsx
+++ app/occasions/page.tsx
@@ -1,3 +1,20 @@
-export default function OccasionsPage() {
-  return <div className="flex-grow">Occasions</div>;
+import { Occasion, columns } from "@/app/occasions/columns";
+import { DataTable } from "@/app/occasions/data-table";
+import { occasions } from "@/lib/placeholder-data";
+
+async function getData(): Promise<Occasion[]> {
+  return occasions;
+}
+
+export default async function OccasionsPage() {
+  const data = await getData();
+
+  return (
+    <div className="flex-grow my-4">
+      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Occasions
+      </h1>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
