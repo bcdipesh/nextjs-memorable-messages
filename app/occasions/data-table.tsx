43e89cb37bb92f96512984401e072aae8709c89a
Dipesh B C <bcdipeshwork@gmail.com>
2024-07-05 10:15:40 -0400

    Improve ui for data table

--- app/occasions/data-table.tsx
+++ app/occasions/data-table.tsx
@@ -52,7 +52,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
-      <div className="flex items-center justify-between py-4">
+      <div className="flex items-center justify-between py-4 space-x-4">
         <Input
           placeholder="Filter emails..."
           value={
