76460a6d92531fc75a2914463c4dfcaeb3f3198b
Dipesh B C <bcdipeshwork@gmail.com>
2024-07-05 21:49:14 -0400

    Update footer styles

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,16 +11,16 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row justify-between">
           <Logo />
           <div className="my-4 md:my-0">
-            <Button variant="link" asChild className="pl-0 font-light">
+            <Button variant="link" asChild className="pl-0">
               <Link href="/terms-of-service">Terms of service</Link>
             </Button>
-            <Button variant="link" asChild className="font-light">
+            <Button variant="link" asChild>
               <Link href="privacy-policy">Privacy notice</Link>
             </Button>
           </div>
         </div>
         <hr className="my-6 border-muted-foreground" />
-        <p className="mt-10 text-xs font-light">
+        <p className="mt-10 text-xs">
           &copy; {currentYear} Memorable Messages. Made to create memories that
           will last a lifetime.
         </p>
