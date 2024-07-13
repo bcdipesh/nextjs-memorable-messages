4f876cdbdfcbfda0a7d8132c76546f5fb6b21452
Dipesh B C <bcdipeshwork@gmail.com>
2024-07-12 22:12:05 -0400

    Setup Flask backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:5328/:path*", // Proxy to Backend
+      },
+    ];
+  },
+};
 
 export default nextConfig;
